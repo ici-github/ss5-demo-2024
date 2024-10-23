2e043d87cddf6df33b120286a30d405e23f04a17
Jerick Carlo Almeda <jerickalmeda@ici.edu.ph>
2024-10-23 06:36:44 +0000

    .

---
+++ htdocs/home.php
@@ -0,0 +1,8 @@
+<?php
+
+    echo "<h1> What is the difference between include and require? </h1>";
+
+    echo "<p> The difference between include and require is that include will only produce a warning (E_WARNING) and the script will continue, while require will produce a fatal error (E_COMPILE_ERROR) and the script will stop. </p>";
+
+    
+?>
